68b1b8177226f35bf07bd232852fc719a966305d
Marmin <38385813+Marmin101@users.noreply.github.com>
2025-01-30 17:17:25 -0500

    Draw Circles with Random Number

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,108 @@
 #include "ofApp.h"
 
+// Declare global variables
+int randomNumbers[5]; // Array to store 5 random numbers
+ofTrueTypeFont fontArial; // Font object
+
+#pragma region Custom Functions
+
+// Generate 5 random numbers between 10 and 100
+static void generateRandomNumbers() {
+	for (int i = 0; i < 5; i++) {
+		randomNumbers[i] = ofRandom(10, 100);
+	}
+}
+
+#pragma endregion
+
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+	generateRandomNumbers();
+	for (int i = 0; i < 5; i++) {
+		std::cout << "Random number " << i + 1 << ": " << randomNumbers[i] << std::endl;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::draw() {
+	// Draw 5 circles with random numbers
+	for (int i = 0; i < 5; i++) {
+		std::string text = std::to_string(randomNumbers[i]);
+		int fontSize = randomNumbers[i];
+		fontArial.load("arial", fontSize);
+		ofRectangle bounds = fontArial.getStringBoundingBox(text, 0, 0);
+
+		int boundsWidth = bounds.getWidth();
+		int boundsHeight = bounds.getHeight();
+		int centerX = 100 + i * 200;
+		int centerY = 200;
+
+		// Circles
+		ofSetColor(50, 200, 50);
+		ofDrawCircle(centerX, centerY, randomNumbers[i]);
+
+		// Text
+		ofSetColor(255, 255, 255);
+		fontArial.drawString(text, centerX - boundsWidth / 2, centerY + boundsHeight / 2);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
